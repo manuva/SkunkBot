ab1164dbb9e6d037b5f81aac25005d94924f114d
manuva <gjtdev@gmail.com>
2022-03-21 13:31:03 -0400

    Update SkunkBotPy.py
    slight reformat

--- SkunkBotPy/SkunkBotPy.py
+++ SkunkBotPy/SkunkBotPy.py
@@ -1,16 +1,17 @@
 #Updated: Now parses messages from stored SQL data
 import os
 import secrets
-from re import X
 import discord
-from dotenv import load_dotenv
 import random
-from discord.ext import commands
 import time
 import pandas
 import re
 #import pymssql #leave out unless using mysql
 import pyodbc
+from re import X
+from ctypes.wintypes import MSG
+from discord.ext import commands
+from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -22,7 +23,9 @@ cursor = CONN.cursor()
 bot = commands.Bot(command_prefix="!")
 client = discord.Client()
 
+############
 ## EVENTS ##
+############
 @client.event
 async def on_ready():
     reportBotInfo()
@@ -32,9 +35,9 @@ async def on_message(message):
     await SendRandomQuote(message)
     
     
-    
-
-## FUNCTIONS ##   
+###############
+## FUNCTIONS ##
+###############   
 async def SendRandomQuote(message):
     if message.author == client.user:
             return
@@ -45,14 +48,14 @@ async def SendRandomQuote(message):
     skunkQuoteText = [row[0] for row in skunkQuotes]
     
     #can probably do this with isalpha or regex. user input 
-    triggerWords = re.compile('[a-zA-Z]')
+    msgUserInput = re.compile('[a-zA-Z]')
     
     #without bot prefix
     #only chat if instance is a DM channel initiated by the user
     if isinstance(message.channel, discord.DMChannel):
-        #true if any words are from user and are in triggerwords 
-        #if any (element in message.content for element in triggerWords):
-        if triggerWords.search(message.content):
+        #true if any words are from user and are in msgUserInput 
+        #if any (element in message.content for element in msgUserInput):
+        if msgUserInput.search(message.content):
             #store a random number 1-5
             randomNumber = secrets.choice(range(1, 5))
             #print('random number is ', randomNumber)
@@ -66,23 +69,14 @@ async def SendRandomQuote(message):
                     await message.channel.send(response)
                     msgCount += 1
                     #print('\n--message count is currently', msgCount)
-                    '''
-            #store and make the response random
-            response = random.choice(skunkQuotes)
-            #indicate the bot is typing
-            async with message.channel.typing():
-                time.sleep(5) #implement a small delay before sending message
-                await message.channel.send(response) #if all are true above, tell the user a random message
-            async with message.channel.typing():
-                response=random.choice(skunkQuotes)
-                time.sleep(3)
-                await message.channel.send(response)
-            async with message.channel.typing():
-                response=random.choice(skunkQuotes)
-                time.sleep(3)
-                await message.channel.send(response)
-'''    
+        else:
+            if isinstance(message.channel, discord.DMChannel):
+                response = 'i dono man'
+                async with message.channel.typing():
+                    time.sleep(secrets.choice(range(1,10)))
+                    await message.channel.send(response)                   
     
+                       
 def reportBotInfo():
     '''for guild in client.guilds:
         if guild.name == GUILD:
@@ -99,7 +93,6 @@ def reportBotInfo():
         f"\n |____/|_|\_\\\__,_|_| |_|_|\_\____/ \___/ \__|"
     )
     
- 
 
 '''
 @bot.command(name='hey', help='responds with a random quote')
@@ -112,18 +105,23 @@ async def skunk_message(ctx):
     response = random.choice(skunkQuotes)
     await ctx.send(response)
  '''
-
-    
    
-
-   
-
-
-
-
-                                              
-
-    
+'''
+        #store and make the response random
+        response = random.choice(skunkQuotes)
+        #indicate the bot is typing
+        async with message.channel.typing():
+            time.sleep(5) #implement a small delay before sending message
+            await message.channel.send(response) #if all are true above, tell the user a random message
+        async with message.channel.typing():
+            response=random.choice(skunkQuotes)
+            time.sleep(3)
+            await message.channel.send(response)
+        async with message.channel.typing():
+            response=random.choice(skunkQuotes)
+            time.sleep(3)
+            await message.channel.send(response)
+'''   
 
 client.run(TOKEN)
 #bot.run(TOKEN)
