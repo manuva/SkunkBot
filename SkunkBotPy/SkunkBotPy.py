7f1108f2e84c74d134a1d796b8b09aa94e9214ef
manuva <gjtdev@gmail.com>
2022-03-21 12:25:17 -0400

    Update SkunkBotPy.py
    Added SQL functionality with ODBC drivers using pyodbc for messaging data

--- SkunkBotPy/SkunkBotPy.py
+++ SkunkBotPy/SkunkBotPy.py
@@ -1,4 +1,4 @@
-#bot.py
+#Updated: Now parses messages from stored SQL data
 import os
 import secrets
 from re import X
@@ -9,10 +9,20 @@ from discord.ext import commands
 import time
 import pandas
 import re
+#import pymssql #leave out unless using mysql
+import pyodbc
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+#CONN = pymssql.connect(DBSERVER, DBUSER, DBPASS, DBNAME)
+CONN = pyodbc.connect(os.getenv('SQL_CONNECTION_STRING'))
+
+cursor = CONN.cursor()
+
+
+
+
 
 bot = commands.Bot(command_prefix="!")
 
@@ -27,83 +37,13 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    skunkQuotes = [
-        'I dono man',
-        'I dono man',
-        'I dono man',
-        'why group when you can solo',
-        'yea it sucks man',
-        'would you be interested in participating in a publicity stunt?',
-        'i feel like thats their gimmick',
-        'only regular people suffer',
-        'so if you immediately go after the truth',
-        'you are a suffering denier',
-        'but its like im not denying the regulars/poor suffer',
-        'im trying to understand why because ultimately we''re in the same position',
-        'it just seems like you gotta be a sociopath to have one world government as an objective',
-        'how many sociopaths can you really get on the same team at one time',
-        'theres a total wine and more store',
-        'yea',
-        'private mega liquor store',
-        'they removed all russian products from their shelves',
-        'in support of ukraine',
-        '..........',
-        'everyones beating around the bush and then just using religion to absolve themselves of guilt and shame',
-        'its weird tho',
-        'and everyone was all about them cause they were high quality and rare',
-        'if these satanists would just stop',
-        'cause cuban cigars were banned',
-        'like we could legit be living in heaven on earth',
-        'which to me is the weird shit about religion',
-        'theres some halve truths in there but nobody will just say it',
-        'tired of the military interventionalism',
-        'thats why i feel like',
-        'their actions in this moment show their hand' ,
-        'you think these nigs are burning zero days',
-        'or using old known shit',
-        'bunch of cucks working at valve',
-        'why is this shit in the stema sale',
-        'no wonder tf2 is fucked up',   
-        'yea but i think its some cheese that',
-	    'youtube is adding "fact checking" to world economic forum references',
-        'makes you think about that guy that was on rogan',
-        'people who have no clue what the wef is see that little snippet and they can change 90% of their minds over time',
-        'or some shit',
-        'this is kind of interesting',
-        'trump canceled pelosi\'s trip to davos on the us taxpayers dime',
-        'back in 2019',
-        'Rick Rule''s VIRTUAL URANIUM Investors Bootcamp',
-        'its a saturday marathon',
-        'damn'
-        'so thats why starlink was deployed in ukraine',
-        'not to give the citizens access to tiktok',
-        'but to give the military access to remote into vehicles',
-        'just peeped that video',
-        'looks like theyre def gonna use peoples starlinks as some sort of mesh network around the jurisdiction',
-        'so that shit dont even gotta jump from orbit to the vehicle it can jump from some dudes roof top if its closer',
-        'star link bout to be the next cyberdyne systems',
-        'so',
-        'the dudes at the top already switched teams',
-        'but the dudes below them are trying to crash land the ship?',
-        'yea idk',
-        'was listening to some shit yesterday',
-        'these nigs were saying theres a beef between the old usa money',
-        'and the old euro money',
-        'the old euro money is the davos nigs with klaus schwab',
-        'the old usa money is the federal reserve nigs',
-        'and then theres the new money that came from old money bribes',
-        'and then theres the new money that came from new era hustlingputin is a new money new hustle guy',
-        'doesnt care about either old money nig',
-        'most of our politicians are new money from old money bribes'
-
-    ]
+    #select sql data
+    skunkQuotes = cursor.execute('SELECT quote_text FROM Quotes').fetchall()
+    #make it a list. updated from static list/array
+    skunkQuoteText = [row[0] for row in skunkQuotes]
     
-    #can do this with isalpha or regex 
+    #can probably do this with isalpha or regex. user input 
     triggerWords = re.compile('[a-zA-Z]')
-        
-    
-    #generate a random number (1-5). that number will determine how many messages are sent per triggerword
-    
     
     #without bot prefix
     #only chat if instance is a DM channel initiated by the user
@@ -111,48 +51,46 @@ async def on_message(message):
         #true if any words are from user and are in triggerwords 
         #if any (element in message.content for element in triggerWords):
         if triggerWords.search(message.content):
+            #store a random number 1-5
             randomNumber = secrets.choice(range(1, 5))
-            print('random number is ', randomNumber)
+            #print('random number is ', randomNumber)
             msgCount = 0
+            #number of messages based on random number
             while (msgCount < randomNumber):
-                response = random.choice(skunkQuotes)
+                response = random.choice(skunkQuoteText)
+                #indicate the bot is typing based on 1-10 seconds. somewhat more realistic
                 async with message.channel.typing():
                     time.sleep(secrets.choice(range(1,10)))
                     await message.channel.send(response)
                     msgCount += 1
-                    print('\n--message count is currently', msgCount)
+                    #print('\n--message count is currently', msgCount)
                     '''
             #store and make the response random
-            response = random.choice(rickQuotes)
+            response = random.choice(skunkQuotes)
             #indicate the bot is typing
             async with message.channel.typing():
                 time.sleep(5) #implement a small delay before sending message
                 await message.channel.send(response) #if all are true above, tell the user a random message
             async with message.channel.typing():
-                response=random.choice(rickQuotes)
+                response=random.choice(skunkQuotes)
                 time.sleep(3)
                 await message.channel.send(response)
             async with message.channel.typing():
-                response=random.choice(rickQuotes)
+                response=random.choice(skunkQuotes)
                 time.sleep(3)
                 await message.channel.send(response)
 '''
 '''
 @bot.command(name='hey', help='responds with a random quote')
-async def rick_message(ctx):
-    rickQuotes = [
+async def skunk_message(ctx):
+    skunkQuotes = [
         'i dono man',
         'why group when you can solo'
     ]
     
-    response = random.choice(rickQuotes)
+    response = random.choice(skunkQuotes)
     await ctx.send(response)
  '''
-    
-def randomizeMessageCount(number):
-    number = random.randint(1,5)
-    return number
-
 
     
 def reportBotInfo():
@@ -171,4 +109,4 @@ def reportBotInfo():
     
 
 client.run(TOKEN)
-bot.run(TOKEN)
+#bot.run(TOKEN)
